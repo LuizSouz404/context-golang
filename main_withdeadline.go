a1897b249388c7cf1880cbde7a04298ce921abdd
LuizSouz404 <luiz.souz404@gmail.com>
2022-12-13 21:54:01 -0300

    add withtimeout example

--- main_withdeadline.go
+++ main_withdeadline.go
@@ -1,33 +1,33 @@
-package main
+// package main
 
-import (
-	"context"
-	"fmt"
-	"time"
-)
+// import (
+// 	"context"
+// 	"fmt"
+// 	"time"
+// )
 
-func main() {
-	ctx, cancel := context.WithDeadline(
-		context.Background(),
-		time.Now().Add(10*time.Second),
-	)
-	fmt.Println(time.Now().Add(10 * time.Second).Format(time.RFC3339))
-	defer cancel()
+// func main() {
+// 	ctx, cancel := context.WithDeadline(
+// 		context.Background(),
+// 		time.Now().Add(10*time.Second),
+// 	)
+// 	fmt.Println(time.Now().Add(10 * time.Second).Format(time.RFC3339))
+// 	defer cancel()
 
-	printUntilCancel(ctx)
-}
+// 	printUntilCancel(ctx)
+// }
 
-func printUntilCancel(ctx context.Context) {
-	count := 0
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("Cancel signal received, exiting...")
-			return
-		default:
-			time.Sleep(1 * time.Second)
-			fmt.Printf("Printing until cancel, number = %d \n", count)
-			count++
-		}
-	}
-}
+// func printUntilCancel(ctx context.Context) {
+// 	count := 0
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			fmt.Println("Cancel signal received, exiting...")
+// 			return
+// 		default:
+// 			time.Sleep(1 * time.Second)
+// 			fmt.Printf("Printing until cancel, number = %d \n", count)
+// 			count++
+// 		}
+// 	}
+// }
