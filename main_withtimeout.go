698dab7e052e2334cf95bf6abc0b109479889960
LuizSouz404 <luiz.souz404@gmail.com>
2022-12-13 21:59:45 -0300

    add withvalue example

--- main_withtimeout.go
+++ main_withtimeout.go
@@ -1,32 +1,32 @@
-package main
+// package main
 
-import (
-	"context"
-	"fmt"
-	"time"
-)
+// import (
+// 	"context"
+// 	"fmt"
+// 	"time"
+// )
 
-func main() {
-	ctx, cancel := context.WithTimeout(
-		context.Background(),
-		10*time.Second,
-	)
-	defer cancel()
+// func main() {
+// 	ctx, cancel := context.WithTimeout(
+// 		context.Background(),
+// 		10*time.Second,
+// 	)
+// 	defer cancel()
 
-	printUntilCancel(ctx)
-}
+// 	printUntilCancel(ctx)
+// }
 
-func printUntilCancel(ctx context.Context) {
-	count := 0
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("Cancel signal received, exiting...")
-			return
-		default:
-			time.Sleep(1 * time.Second)
-			fmt.Printf("Printing until cancel, number = %d \n", count)
-			count++
-		}
-	}
-}
+// func printUntilCancel(ctx context.Context) {
+// 	count := 0
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			fmt.Println("Cancel signal received, exiting...")
+// 			return
+// 		default:
+// 			time.Sleep(1 * time.Second)
+// 			fmt.Printf("Printing until cancel, number = %d \n", count)
+// 			count++
+// 		}
+// 	}
+// }
