af336147cc30e2d88a2d0cec0d8ccc546dfea64d
LuizSouz404 <luiz.souz404@gmail.com>
2022-12-13 21:42:28 -0300

    add withcancel example

--- main_basics.go
+++ main_basics.go
@@ -1,40 +1,40 @@
-package main
+// package main
 
-import (
-	"context"
-	"fmt"
-	"time"
-)
+// import (
+// 	"context"
+// 	"fmt"
+// 	"time"
+// )
 
-func numeros(v chan<- int) {
-	for i := 0; i < 10; i++ {
-		v <- i
-		fmt.Printf("número %d escrito no channel \n", i)
-	}
-	close(v)
-}
+// func numeros(v chan<- int) {
+// 	for i := 0; i < 10; i++ {
+// 		v <- i
+// 		fmt.Printf("número %d escrito no channel \n", i)
+// 	}
+// 	close(v)
+// }
 
-func main() {
-	ctx, cf := context.WithCancel(context.Background())
-	go func() {
-		time.Sleep(time.Second * 5)
-		cf()
-		fmt.Println("Timeout!")
-	}()
+// func main() {
+// 	ctx, cf := context.WithCancel(context.Background())
+// 	go func() {
+// 		time.Sleep(time.Second * 5)
+// 		cf()
+// 		fmt.Println("Timeout!")
+// 	}()
 
-	c := make(chan int, 3)
-	go numeros(c)
+// 	c := make(chan int, 3)
+// 	go numeros(c)
 
-loopNum:
-	for {
-		select {
-		case <-ctx.Done():
-			break loopNum
-		case v, ok := <-c:
-			if ok {
-				fmt.Printf("número %d lido no channel \n", v)
-				time.Sleep(2 * time.Second)
-			}
-		}
-	}
-}
+// loopNum:
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			break loopNum
+// 		case v, ok := <-c:
+// 			if ok {
+// 				fmt.Printf("número %d lido no channel \n", v)
+// 				time.Sleep(2 * time.Second)
+// 			}
+// 		}
+// 	}
+// }
