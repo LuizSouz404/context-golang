27143473a1fd14f0d170c16f9ec8b0dcfd93ea0c
LuizSouz404 <luiz.souz404@gmail.com>
2022-12-13 21:48:19 -0300

    add withdeadline example

---
+++ main_withdeadline.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithDeadline(
+		context.Background(),
+		time.Now().Add(10*time.Second),
+	)
+	fmt.Println(time.Now().Add(10 * time.Second).Format(time.RFC3339))
+	defer cancel()
+
+	printUntilCancel(ctx)
+}
+
+func printUntilCancel(ctx context.Context) {
+	count := 0
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Cancel signal received, exiting...")
+			return
+		default:
+			time.Sleep(1 * time.Second)
+			fmt.Printf("Printing until cancel, number = %d \n", count)
+			count++
+		}
+	}
+}
